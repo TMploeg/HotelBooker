f68903ac3b29b66fe27b310f0cedaaec0d9aaba5
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-11 11:32:37 +0100

    Update BookingController.java
    added GetById and converted response type to ResponseEntity

--- src/main/java/com/tmploeg/hotelbooker/BookingController.java
+++ src/main/java/com/tmploeg/hotelbooker/BookingController.java
@@ -1,28 +1,46 @@
 package com.tmploeg.hotelbooker;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("bookings")
 public class BookingController {
-    public record Booking(String ownerName, LocalDateTime timeslot){}
+    public record Booking(int id, String ownerName, LocalDateTime timeslot){}
 
-    @GetMapping("")
-    public List<Booking> getAll(){
+    private final List<Booking> bookings;
+
+    public BookingController(){
         int count = 10;
 
-        List<Booking> bookings = new ArrayList<>(count);
+        bookings = new ArrayList<>(count);
 
         for(int i = 0; i < count; i++){
-            bookings.add(new Booking("person_" + i, LocalDateTime.now()));
+            bookings.add(new Booking(i + 1, "person_" + i, LocalDateTime.now()));
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Booking>> getAll(){
+        return ResponseEntity.ok(bookings);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Booking> getById(@PathVariable int id){
+        for(Booking booking : bookings){
+            if(booking.id() == id){
+                return ResponseEntity.ok(booking);
+            }
         }
 
-        return bookings;
+        return ResponseEntity.notFound().build();
     }
 }
