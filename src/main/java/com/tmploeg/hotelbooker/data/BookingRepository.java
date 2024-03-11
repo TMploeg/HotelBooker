f50c191373872a2a3a900c280eeeb23f840f77ff
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-11 14:47:46 +0100

    Update BookingRepository.java
    added findByOwnerName

--- src/main/java/com/tmploeg/hotelbooker/data/BookingRepository.java
+++ src/main/java/com/tmploeg/hotelbooker/data/BookingRepository.java
@@ -3,4 +3,8 @@ package com.tmploeg.hotelbooker.data;
 import com.tmploeg.hotelbooker.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByOwnerName(String ownerName);
+}
