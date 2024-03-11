973f250806efae4f43c04b17558c640a2061a653
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-11 14:48:51 +0100

    Update BookingController.java
    update addBooking

--- src/main/java/com/tmploeg/hotelbooker/BookingController.java
+++ src/main/java/com/tmploeg/hotelbooker/BookingController.java
@@ -4,9 +4,12 @@ import com.tmploeg.hotelbooker.data.BookingRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import com.tmploeg.hotelbooker.models.Booking;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("bookings")
@@ -35,9 +38,27 @@ public class BookingController {
     }
 
     @PostMapping("add")
-    public ResponseEntity<Void> addBooking(@RequestBody Booking booking){
+    public ResponseEntity<Booking> addBooking(@RequestBody Booking booking, UriComponentsBuilder ucb){
         bookingRepository.save(booking);
 
-        return ResponseEntity.noContent().build();
+        URI newBookingLocation = ucb
+                .path("{id}")
+                .buildAndExpand(booking.getId())
+                .toUri();
+
+        return ResponseEntity.created(newBookingLocation).body(booking);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteBooking(@PathVariable long id){
+        Optional<Booking> deleteBooking = bookingRepository.findById(id);
+
+        if(deleteBooking.isPresent()){
+            bookingRepository.delete(deleteBooking.get());
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
