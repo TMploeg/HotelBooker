0ce5146d81ca74f193b577dcfdcb0c48f133d3fd
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-11 10:40:37 +0100

    initial commit

---
+++ src/test/java/com/tmploeg/hotelbooker/HotelBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmploeg.hotelbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
