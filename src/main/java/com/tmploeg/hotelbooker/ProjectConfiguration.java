7f0a2becd681bded89ec3c276431a39bd37aa6f2
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-15 20:09:39 +0100

    Update ProjectConfiguration.java
    added PasswordEncoder dependency

--- src/main/java/com/tmploeg/hotelbooker/ProjectConfiguration.java
+++ src/main/java/com/tmploeg/hotelbooker/ProjectConfiguration.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
@@ -11,5 +13,10 @@ public class ProjectConfiguration {
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
