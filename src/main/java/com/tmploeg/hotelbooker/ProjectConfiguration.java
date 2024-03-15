c0fab444ac94fb4b992647e94ef3f694caa87c4d
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-15 20:11:13 +0100

    Update ProjectConfiguration.java
    added SecurityFilterChain dependency

--- src/main/java/com/tmploeg/hotelbooker/ProjectConfiguration.java
+++ src/main/java/com/tmploeg/hotelbooker/ProjectConfiguration.java
@@ -3,20 +3,31 @@ package com.tmploeg.hotelbooker;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfiguration {
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
+  @Bean
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .httpBasic(Customizer.withDefaults())
+        .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+        .build();
+  }
 }
