09dc72b274d26f1f7f8b36bf1b2fbcda41677434
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-15 20:05:43 +0100

    Update UserRepository.java
    fixed id type

--- src/main/java/com/tmploeg/hotelbooker/data/UserRepository.java
+++ src/main/java/com/tmploeg/hotelbooker/data/UserRepository.java
@@ -2,9 +2,8 @@ package com.tmploeg.hotelbooker.data;
 
 import com.tmploeg.hotelbooker.models.User;
 import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, String> {
   Optional<User> findByUsername(String username);
 }
