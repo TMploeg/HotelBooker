a7895c03a1944d30a4b6780aa80b0d79b832be77
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-16 20:27:58 +0100

    moved admin default password to application.properties

--- src/main/java/com/tmploeg/hotelbooker/data/Seeder.java
+++ src/main/java/com/tmploeg/hotelbooker/data/Seeder.java
@@ -2,13 +2,12 @@ package com.tmploeg.hotelbooker.data;
 
 import com.tmploeg.hotelbooker.enums.RoleName;
 import com.tmploeg.hotelbooker.models.Role;
-import com.tmploeg.hotelbooker.models.User;
 import com.tmploeg.hotelbooker.services.RoleService;
 import com.tmploeg.hotelbooker.services.UserService;
 import java.util.Arrays;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +15,9 @@ import org.springframework.stereotype.Component;
 public class Seeder implements CommandLineRunner {
   private final UserService userService;
   private final RoleService roleService;
+  private final Environment environment;
+
+  private static final String ADMIN_PASSWORD_ENVIRONMENT_KEY = "hotelbooker.admin-password";
 
   @Override
   public void run(String... args) throws Exception {
@@ -28,15 +30,9 @@ public class Seeder implements CommandLineRunner {
   }
 
   private void seedUsers() {
-    Role adminRole = roleService.getByName(RoleName.ADMIN);
-    Set<User> adminUsers = userService.findByRole(adminRole);
-
-    if (!adminUsers.isEmpty()) {
-      return;
-    }
-
     String adminUsername = "ADMIN";
-    String adminPassword = "DigitalGold#0153";
+    String adminPassword = environment.getProperty(ADMIN_PASSWORD_ENVIRONMENT_KEY);
+    Role adminRole = roleService.getByName(RoleName.ADMIN);
 
     userService.save(adminUsername, adminPassword, adminRole);
   }
