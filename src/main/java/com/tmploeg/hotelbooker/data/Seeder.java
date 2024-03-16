52f9073e554124ec774de65729bb74f948c3a0ac
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-16 20:48:50 +0100

    fixed admin credentials

--- src/main/java/com/tmploeg/hotelbooker/data/Seeder.java
+++ src/main/java/com/tmploeg/hotelbooker/data/Seeder.java
@@ -2,20 +2,23 @@ package com.tmploeg.hotelbooker.data;
 
 import com.tmploeg.hotelbooker.enums.RoleName;
 import com.tmploeg.hotelbooker.models.Role;
-import com.tmploeg.hotelbooker.models.User;
 import com.tmploeg.hotelbooker.services.RoleService;
 import com.tmploeg.hotelbooker.services.UserService;
 import java.util.Arrays;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class Seeder implements CommandLineRunner {
+  private static final String ADMIN_USERNAME_ENVIRONMENT_KEY = "hotelbooker-admin-username";
+  private static final String ADMIN_PASSWORD_ENVIRONMENT_KEY = "hotelbooker-admin-password";
+
   private final UserService userService;
   private final RoleService roleService;
+  private final Environment environment;
 
   @Override
   public void run(String... args) throws Exception {
@@ -28,15 +31,9 @@ public class Seeder implements CommandLineRunner {
   }
 
   private void seedUsers() {
+    String adminUsername = environment.getProperty(ADMIN_USERNAME_ENVIRONMENT_KEY);
+    String adminPassword = environment.getProperty(ADMIN_PASSWORD_ENVIRONMENT_KEY);
     Role adminRole = roleService.getByName(RoleName.ADMIN);
-    Set<User> adminUsers = userService.findByRole(adminRole);
-
-    if (!adminUsers.isEmpty()) {
-      return;
-    }
-
-    String adminUsername = "ADMIN";
-    String adminPassword = "DigitalGold#0153";
 
     userService.save(adminUsername, adminPassword, adminRole);
   }
