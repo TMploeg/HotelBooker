1d2dc2cf77b831792a3e29856c777b7743319bc3
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-17 17:48:18 +0100

    added repository annotations

--- src/main/java/com/tmploeg/hotelbooker/data/RoleRepository.java
+++ src/main/java/com/tmploeg/hotelbooker/data/RoleRepository.java
@@ -3,7 +3,9 @@ package com.tmploeg.hotelbooker.data;
 import com.tmploeg.hotelbooker.models.Role;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, String> {
   public Optional<Role> findByName(String name);
 }
--- src/main/java/com/tmploeg/hotelbooker/data/UserRepository.java
+++ src/main/java/com/tmploeg/hotelbooker/data/UserRepository.java
@@ -5,8 +5,11 @@ import com.tmploeg.hotelbooker.models.User;
 import java.util.Optional;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
   Optional<User> findByUsername(String username);
+
   Set<User> findByRole(Role role);
 }
