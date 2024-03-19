783108a1873c5604e61f836b5444fc9bd66f6b0e
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-19 15:13:14 +0100

    updated repositories

--- src/main/java/com/tmploeg/hotelbooker/data/HotelRepository.java
+++ src/main/java/com/tmploeg/hotelbooker/data/HotelRepository.java
@@ -3,4 +3,8 @@ package com.tmploeg.hotelbooker.data;
 import com.tmploeg.hotelbooker.models.entities.Hotel;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HotelRepository extends CrudRepository<Hotel, Long> {}
+import java.util.Set;
+
+public interface HotelRepository extends CrudRepository<Hotel, Long> {
+  Set<Hotel> findByOrderById();
+}
