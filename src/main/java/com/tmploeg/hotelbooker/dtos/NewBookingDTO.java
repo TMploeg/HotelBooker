2f2795594bd3d6a40a96d7afc76ce93e411ed4ab
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-03-19 15:14:10 +0100

    Update NewBookingDTO.java

--- src/main/java/com/tmploeg/hotelbooker/dtos/NewBookingDTO.java
+++ src/main/java/com/tmploeg/hotelbooker/dtos/NewBookingDTO.java
@@ -1,6 +1,4 @@
 package com.tmploeg.hotelbooker.dtos;
 
-import com.tmploeg.hotelbooker.models.RoomNumber;
-
 public record NewBookingDTO(
-    Long id, String checkIn, String checkOut, Long hotelId, RoomNumber[] roomNumbers) {}
+    Long id, String checkIn, String checkOut, Long hotelId, Integer[] roomNumbers) {}
