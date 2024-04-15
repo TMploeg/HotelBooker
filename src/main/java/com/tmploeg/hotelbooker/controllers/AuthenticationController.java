e99f8752f5d4cbeddee435aa639fdb8b4d193863
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-15 13:11:35 +0200

    Update AuthenticationController.java
    added reqister endpoint

--- src/main/java/com/tmploeg/hotelbooker/controllers/AuthenticationController.java
+++ src/main/java/com/tmploeg/hotelbooker/controllers/AuthenticationController.java
@@ -1,8 +1,13 @@
 package com.tmploeg.hotelbooker.controllers;
 
 import com.tmploeg.hotelbooker.dtos.AuthDTO;
+import com.tmploeg.hotelbooker.dtos.UserDTO;
+import com.tmploeg.hotelbooker.enums.RoleName;
 import com.tmploeg.hotelbooker.exceptions.BadRequestException;
+import com.tmploeg.hotelbooker.models.entities.User;
 import com.tmploeg.hotelbooker.services.JwtService;
+import com.tmploeg.hotelbooker.services.RoleService;
+import com.tmploeg.hotelbooker.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("auth")
@@ -18,9 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
+  private final UserService userService;
+  private final RoleService roleService;
 
   @PostMapping("login")
   public String login(@RequestBody AuthDTO authDTO) {
+    if (authDTO.username() == null || authDTO.password() == null) {
+      throw new BadRequestException("username and password are required");
+    }
+
     try {
       authenticationManager.authenticate(
           new UsernamePasswordAuthenticationToken(authDTO.username(), authDTO.password()));
@@ -29,5 +41,23 @@ public class AuthenticationController {
     }
 
     return jwtService.generateTokenForUser(authDTO.username());
+  }
+
+  @PostMapping("register")
+  public UserDTO register(@RequestBody AuthDTO authDTO, UriComponentsBuilder ucb) {
+    if (authDTO.username() == null || authDTO.password() == null) {
+      throw new BadRequestException("username and password are required");
+    }
+
+    if (userService.userExists(authDTO.username())
+        || !userService.isValidPassword(authDTO.password())) {
+      throw new BadRequestException("username or password is invalid");
+    }
+
+    User user =
+        userService.save(
+            authDTO.username(), authDTO.password(), roleService.findByName(RoleName.USER));
+
+    return UserDTO.fromUser(user);
   }
 }
