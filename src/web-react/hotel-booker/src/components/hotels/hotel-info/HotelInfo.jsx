ce5d4863c707ffb21cd66663794c9ef64bd6434d
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-23 21:24:49 +0200

    Update HotelInfo.jsx

--- src/web-react/hotel-booker/src/components/hotels/hotel-info/HotelInfo.jsx
+++ src/web-react/hotel-booker/src/components/hotels/hotel-info/HotelInfo.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ApiService from "../../../services/ApiService.js";
 
 export default function HotelInfo() {
     const { id } = useParams();
+    const apiService = new ApiService();
+    const [hotel, setHotel] = useState(null);
+
+    useEffect(() => loadHotel, []);
+
+    if (hotel) {
+        return <div>
+            <h1 className="hotel-name">{hotel.name}</h1>
+            <div className="hotel-address">{hotel.address}</div>
+        </div>
+    }
+
     return <div>
-        <h1>{id}</h1>
+        Loading...
     </div>
+
+    function loadHotel() {
+        apiService
+            .get(`hotels/${id}`)
+            .onComplete(response => setHotel(response.body))
+    }
 }
