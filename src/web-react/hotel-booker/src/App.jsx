e36e104794acf0fc878d4200d6347e28fc0d6161
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-23 21:24:24 +0200

    added home component

--- src/web-react/hotel-booker/src/App.jsx
+++ src/web-react/hotel-booker/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home";
 import HotelInfo from "./components/hotels/hotel-info";
 import HotelList from "./components/hotels/hotel-list";
 
@@ -8,8 +9,9 @@ export default function App() {
     <div className="app-container">
       <BrowserRouter>
         <Routes>
-          <Route path="hotels" element={<HotelList />} />
-          <Route path="hotels/:id" element={<HotelInfo />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<HotelList />} />
+          <Route path="/hotels/:id" element={<HotelInfo />} />
         </Routes>
       </BrowserRouter>
     </div>
