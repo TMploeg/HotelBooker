dcf3ddbc95a5054c6252c3490e85c28ebfe6ce95
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-23 15:52:18 +0200

    created hotel list and basic hotel info components

---
+++ src/web-react/hotel-booker/src/services/ApiService.js
@@ -0,0 +1,59 @@
+export default class ApiService {
+    get(url, params) {
+        return new ApiRequest('GET', url, params);
+    }
+}
+
+export class ApiRequest {
+    #REQUEST_COMPLETED_STATE = 4;
+
+    #request;
+    #responseData;
+    #onComplete;
+
+    constructor(httpMethod, url, params) {
+        const joinedParams = params === undefined ? undefined : Object.keys(params).map(key => `${key}=${params[key]}`).join('&');
+
+        this.#request = new XMLHttpRequest();
+        this.#request.open(httpMethod, url + (joinedParams !== undefined && joinedParams.length > 0 ? `?${joinedParams}` : ''));
+        this.#request.setRequestHeader('accept', 'application/json');
+        this.#request.onreadystatechange = () => {
+            if (!this.#isRequestComplete(this.#request)) {
+                return;
+            }
+
+            this.#responseData = this.#isRequestSuccesfull(this.#request)
+                ? {
+                    succes: true,
+                    body: JSON.parse(this.#request.responseText)
+                }
+                : {
+                    succes: false,
+                    message: this.#request.responsetext
+                };
+
+            if (this.#onComplete) {
+                this.#onComplete(this.#responseData);
+            }
+        }
+
+        this.#request.send();
+    }
+
+    onComplete(onComplete) {
+        if (this.#responseData) {
+            onComplete(this.#responseData);
+        }
+        else {
+            this.#onComplete = onComplete;
+        }
+    }
+
+    #isRequestComplete(request) {
+        return request.readyState === this.#REQUEST_COMPLETED_STATE;
+    }
+
+    #isRequestSuccesfull(request) {
+        return Math.floor(request.status / 100) === 2;
+    }
+}
