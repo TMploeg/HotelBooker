3a2a01789de1eae6d927bf48fd282da60de33de8
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-28 20:45:40 +0200

    added password visibility toggle

--- src/web-react/hotel-booker/src/components/auth/AuthForm.jsx
+++ src/web-react/hotel-booker/src/components/auth/AuthForm.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import FlatButton from "../general/flat-button/FlatButton";
+import IconButton from "../general/icon-button";
 import InputField from "../general/input-field/InputField";
 import "./AuthForm.css";
 
 export default function AuthForm({ title, onSubmit }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState(false);
 
     const titleElement = title ? <h1>{title}</h1> : null;
 
     return <div className="form-container">
         {titleElement}
         <form className="auth-form">
-            <InputField label="Username" value={username} onValueChanged={setUsername} />
-            <InputField label="Password" value={password} onValueChanged={setPassword} />
+            <div className="input-field-container">
+                <InputField label="Username" value={username} onValueChanged={setUsername} />
+            </div>
+            <div className="input-field-container">
+                <InputField
+                    label="Password"
+                    value={password}
+                    onValueChanged={setPassword}
+                    type={passwordVisible ? 'text' : 'password'} />
+                <div className="suffix">
+                    <IconButton
+                        onClick={() => setPasswordVisible(visible => !visible)}
+                        imgUrl={`/images/visibility_${passwordVisible ? 'off' : 'on'}.png`} />
+                </div>
+            </div>
             <FlatButton onClick={() => onSubmit(username, password)}>Submit</FlatButton>
         </form>
     </div>
