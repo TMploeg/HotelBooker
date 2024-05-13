caa3bb28afce845ad0da95c4d085a475c894e178
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-13 14:09:39 +0200

    improved validation

--- src/web-react/hotel-booker/src/components/auth/AuthForm.jsx
+++ src/web-react/hotel-booker/src/components/auth/AuthForm.jsx
@@ -3,12 +3,12 @@ import FlatButton from "../general/flat-button/FlatButton";
 import InputField from "../general/input-field/InputField";
 import "./AuthForm.css";
 
-export default function AuthForm({ title, onSubmit }) {
+export default function AuthForm({ title, onSubmit, validateUsername, validatePassword }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const usernameErrors = getUsernameErrors();
-    const passwordErrors = getPasswordErrors();
+    const usernameErrors = validateUsername ? validateUsername(username) : [];
+    const passwordErrors = validatePassword ? validatePassword(password) : [];
 
     const titleElement = title ? <h1>{title}</h1> : null;
 
@@ -31,40 +31,4 @@ export default function AuthForm({ title, onSubmit }) {
                 disabled={usernameErrors.length > 0 || passwordErrors.length > 0}>Submit</FlatButton>
         </form>
     </div>
-
-    function getUsernameErrors() {
-        const errors = [];
-        if (username.length === 0) {
-            errors.push('username is required');
-        }
-        return errors;
-    }
-
-    function getPasswordErrors() {
-        const errors = [];
-        const passwordMinLength = 7;
-        const specialCharacters = '!@#$%&*()_+=|<>?{}\\[\\]~-';
-
-        if (password.length < passwordMinLength) {
-            errors.push(`must have at least ${passwordMinLength} characters`);
-        }
-        if (!regEx(password, '[a-z]')) {
-            errors.push('must have lowercase letter');
-        }
-        if (!regEx(password, '[A-Z]')) {
-            errors.push('must have uppercase letter');
-        }
-        if (!regEx(password, '[0-9]')) {
-            errors.push('must have digit');
-        }
-        if (!regEx(password, `[${specialCharacters}]`)) {
-            errors.push('must have special character');
-        }
-
-        return errors;
-    }
-
-    function regEx(text, pattern) {
-        return text.search(pattern) >= 0;
-    }
 }
--- src/web-react/hotel-booker/src/components/auth/login/Login.jsx
+++ src/web-react/hotel-booker/src/components/auth/login/Login.jsx
@@ -2,17 +2,37 @@ import UserService from "../../../services/UserService.js";
 import AuthForm from "../AuthForm";
 
 export default function Login({ onLogin }) {
-    return <AuthForm title="Login to Account" onSubmit={login} />
+    return <AuthForm
+        title="Login to Account"
+        onSubmit={login}
+        validateUsername={validateUsername}
+        validatePassword={validatePassword} />
 
     function login(username, password) {
         UserService.login(username, password)
             .then(
-                response => {
-                    if (onLogin) {
-                        onLogin();
-                    }
-                },
+                () => onLogin?.(),
                 console.error
             );
+    }
+
+    function validateUsername(username) {
+        const errors = [];
+
+        if (username === undefined || username === null || username.length === 0) {
+            errors.push('username is required');
+        }
+
+        return errors;
+    }
+
+    function validatePassword(password) {
+        const errors = [];
+
+        if (password === undefined || password === null || password.length === 0) {
+            errors.push('password is required');
+        }
+
+        return errors;
     }
 }
